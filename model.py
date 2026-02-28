3371a67322b9a6139add7385d682a640eb2eb083
m-heshmat <mohamedheshmat805@gmail.com>
2026-02-28 11:45:55 +0200

    push model.py
    added a snippet of the model.py

---
+++ model.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense
+from tensorflow.keras.models import Model
+
+input_shape = (32, 32, 3) # Example input shape
+base_model = ResNet50(weights='imagenet', include_top=False, input_shape=input_shape)
+
+base_model.trainable = False
