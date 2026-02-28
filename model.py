0f9c8ad9d7267c062d26a52dc0f1dfa80b1976b5
nourwalid12abdal-blip <nourwalid12abdal@gmail.com>
2026-02-28 12:13:17 +0200

    ...

--- model.py
+++ model.py
@@ -1,11 +1,16 @@
-import tensorflow as tf
-from tensorflow.keras.applications import ResNet50
-from tensorflow.keras.layers import GlobalAveragePooling2D, Dense
-from tensorflow.keras.models import Model
 
-input_shape = (32, 32, 3) # Example input shape
-base_model = ResNet50(weights='imagenet', include_top=False, input_shape=input_shape)
 
+<<<<<<< Updated upstream
 base_model.trainable = False
 
 ""
+=======
+x = GlobalAveragePooling2D()(base_model.output)
+output = Dense(10, activation='softmax')(x)
+model = Model(inputs=base_model.input, outputs=output)
+
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit(x_train_preprocessed, y_train_categorical, epochs=5, batch_size=128)
+>>>>>>> Stashed changes
